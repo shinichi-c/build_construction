4e644d7771c8ef84e984e2a5d7bbce6924b6904d
Zamasu <129572513+zamasu0@users.noreply.github.com>
2024-08-17 23:36:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -39,7 +39,6 @@ git clone https://github.com/zamasu0/android_frameworks_base --depth=1 framework
 git clone https://github.com/zamasu0/android_build --depth=1 build/make
 
 # build-setup
-export LINEAGE_BUILD=true
 source build/envsetup.sh
-lunch lineage_fajita-ap2a-eng
+lunch lineage_fajita-ap2a-userdebug
 m bacon
