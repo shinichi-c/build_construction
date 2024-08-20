e2c6c3b82a9f1912fb72d84d228311bbfce61c5c
Zamasu <129572513+zamasu0@users.noreply.github.com>
2024-08-20 23:49:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ git clone https://github.com/zamasu0/android_device_oneplus_fajita_4.19 --depth=
 git clone https://github.com/zamasu0/android_device_oneplus_sdm845-common --depth=1 -b pxl_os device/oneplus/sdm845-common
 git clone https://github.com/zamasu0/frameworks_native --depth=1 frameworks/native
 git clone https://github.com/zamasu0/frameworks_base --depth=1 frameworks/base
-git clone https://github.com/LineageOS/android_hardware_oneplus --depth=1 hardware/oneplus
+git clone https://github.com/zamasu0/android_hardware_oneplus --depth=1 hardware/oneplus
 
 # build-setup
 source build/envsetup.sh
