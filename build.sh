6423bff35040773dc6a1dcab26ad39828be5b279
Zamasu <129572513+zamasu0@users.noreply.github.com>
2024-08-25 22:46:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,20 +21,20 @@ rm -rf vendor/lindroid
 rm -rf external/lxc
 
 # Clone repositories
-git clone https://github.com/Terminator-J/crdroid_kernel_oneplus_sdm845 --depth=1 -b 14.0-4.19-test2 kernel/oneplus/sdm845
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 vendor/oneplus/fajita
-git clone https://github.com/zamasu0/proprietary_vendor_oneplus_sdm845-common_4.19 --depth=1 vendor/oneplus/sdm845-common
-git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los --depth=1 device/pixelstar/sepolicy
-git clone https://github.com/zamasu0/android_device_qcom_sepolicy_vndr_4.19_los --depth=1 device/qcom/sepolicy_vndr/legacy-um
-git clone https://github.com/zamasu0/media --depth=1 hardware/qcom-caf/sdm845/media
-git clone https://github.com/zamasu0/audio --depth=1 hardware/qcom-caf/sdm845/audio
-git clone https://github.com/zamasu0/display --depth=1 hardware/qcom-caf/sdm845/display
-git clone https://github.com/Linux-on-droid/vendor_lindroid --depth=1 vendor/lindroid
-git clone https://github.com/Linux-on-droid/libhybris --depth=1 -b staging/lindroid-21 external/libhybris
-git clone https://github.com/Linux-on-droid/external_lxc --depth=1 external/lxc
-git clone https://github.com/zamasu0/android_device_oneplus_fajita_4.19 --depth=1 -b pxl_s device/oneplus/fajita
-git clone https://github.com/zamasu0/android_device_oneplus_sdm845-common --depth=1 -b pxl_s device/oneplus/sdm845-common
-git clone https://github.com/zamasu0/android_hardware_oneplus --depth=1 hardware/oneplus
+git clone https://github.com/Terminator-J/crdroid_kernel_oneplus_sdm845.git --depth=1 -b 14.0-4.19-test2 kernel/oneplus/sdm845
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita.git --depth=1 vendor/oneplus/fajita
+git clone https://github.com/zamasu0/proprietary_vendor_oneplus_sdm845-common_4.19.git --depth=1 vendor/oneplus/sdm845-common
+git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los.git --depth=1 device/pixelstar/sepolicy
+git clone https://github.com/zamasu0/android_device_qcom_sepolicy_vndr_4.19_los.git --depth=1 device/qcom/sepolicy_vndr/legacy-um
+git clone https://github.com/zamasu0/media.git --depth=1 hardware/qcom-caf/sdm845/media
+git clone https://github.com/zamasu0/audio.git --depth=1 hardware/qcom-caf/sdm845/audio
+git clone https://github.com/zamasu0/display.git --depth=1 hardware/qcom-caf/sdm845/display
+git clone https://github.com/Linux-on-droid/vendor_lindroid.git --depth=1 vendor/lindroid
+git clone https://github.com/Linux-on-droid/libhybris.git --depth=1 -b staging/lindroid-21 external/libhybris
+git clone https://github.com/Linux-on-droid/external_lxc.git --depth=1 external/lxc
+git clone https://github.com/zamasu0/proprietary_vendor_oneplus_fajita_4.19.git --depth=1 -b pxl_s device/oneplus/fajita
+git clone https://github.com/zamasu0/android_device_oneplus_sdm845-common.git --depth=1 -b pxl_s device/oneplus/sdm845-common
+git clone https://github.com/zamasu0/android_hardware_oneplus.git --depth=1 hardware/oneplus
 
 # build-setup
 source build/envsetup.sh
