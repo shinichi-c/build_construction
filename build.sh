091e92ed159a9777a6f3cf4371b49ba330f2ab54
kudo <Gokus12456@gmail.com>
2024-10-09 08:23:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ rm -r vendor/gms/common/common-vendor.mk
 # Clone repositories
 git clone https://github.com/Terminator-J/crdroid_kernel_oneplus_sdm845 --depth=1 -b 14.0-4.19-test2 kernel/oneplus/sdm845
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 vendor/oneplus/fajita
-git clone https://github.com/zamasu0/proprietary_vendor_oneplus_sdm845-common_4.19 --depth=1 vendor/oneplus/sdm845-common
+git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common --depth=1 vendor/oneplus/sdm845-common
 git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los --depth=1 device/lineage/sepolicy
 git clone https://github.com/zamasu0/android_device_qcom_sepolicy_vndr_4.19_los --depth=1 device/qcom/sepolicy_vndr/legacy-um
 git clone https://github.com/zamasu0/media --depth=1 hardware/qcom-caf/sdm845/media
