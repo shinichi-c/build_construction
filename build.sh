f9072785b6f8c2e1877144f67a1e08d5db6374df
kudo <Gokus12456@gmail.com>
2024-10-17 13:34:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ rm -rf hardware/oneplus
 # Clone repositories
 git clone https://github.com/uwu-gl/kernel_oneplus_sdm845 --depth=1 -b fifteen kernel/oneplus/sdm845
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 vendor/oneplus/fajita
-git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common --depth=1 vendor/oneplus/sdm845-common
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sdm845-common --depth=1 vendor/oneplus/sdm845-common
 git clone https://github.com/uwu-gl/device_oneplus_fajita --depth=1 -b fifteen device/oneplus/fajita
 git clone https://github.com/uwu-gl/device_oneplus_sdm845-common --depth=1 -b fifteen device/oneplus/sdm845-common
 git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 hardware/oneplus
