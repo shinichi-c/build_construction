bc7de184b849aeac4efa4d1ad07829f7ba24b9f8
kudo <Gokus12456@gmail.com>
2024-10-19 23:50:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 # Remove existing directories
+
 rm -rf device/oneplus/fajita
 rm -rf device/oneplus/sdm845-common
-rm -rf device/lineage/sepolicy
-rm -rf device/qcom/sepolicy_vndr/legacy-um
-rm -rf kernel/oneplus/sdm845
 rm -rf vendor/oneplus/fajita
 rm -rf vendor/oneplus/sdm845-common
+rm -rf kernel/oneplus/sdm845
 rm -rf hardware/oneplus
-rm -rf hardware/qcom-caf/sdm845/media
-rm -rf hardware/qcom-caf/sdm845/audio
-rm -rf hardware/qcom-caf/sdm845/display
-rm -rf frameworks/native
-rm -rf frameworks/base
-rm -rf external/libhybris
-rm -rf vendor/lindroid
-rm -rf external/lxc
+
+git clone https://github.com/LineageOS/android_device_oneplus_fajita --depth=1 -b lineage-21 device/oneplus/fajita
+
+
 
 # Clone repositories
 git clone https://github.com/Terminator-J/crdroid_kernel_oneplus_sdm845 --depth=1 -b 14.0-4.19-test2 kernel/oneplus/sdm845
@@ -30,10 +25,8 @@ git clone https://github.com/shinichi-c/display --depth=1 -b 14.0-4.19 hardware/
 git clone https://github.com/Linux-on-droid/vendor_lindroid --depth=1 -b lindroid-21 vendor/lindroid
 git clone https://github.com/Linux-on-droid/libhybris --depth=1 -b lindroid-21 external/libhybris
 git clone https://github.com/Linux-on-droid/external_lxc --depth=1 -b lindroid-21 external/lxc
-git clone https://github.com/shinichi-c/android_device_oneplus_fajita --depth=1 -b lineage-21 device/oneplus/fajita
+
 git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common --depth=1 -b pxl_os device/oneplus/sdm845-common
-git clone https://github.com/shinichi-c/frameworks_native_pxl --depth=1 -b fourteen frameworks/native
-git clone https://github.com/shinichi-c/frameworks_base --depth=1 -b fourteen frameworks/base
 git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 -b lineage-21 hardware/oneplus
 
 source build/envsetup.sh
