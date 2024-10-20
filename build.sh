bf43fbe51fb9c46291c438e66f5553f700903ea5
kudo <Gokus12456@gmail.com>
2024-10-20 08:41:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,15 @@
-#!/bin/bash
+rm -rf .repo/local_manifests/
 
-# Cleanup to fix SyncErrors raised during branch checkouts
+# Repo Init
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs
 
-rm -rf packages/apps/Calendar
-rm -rf proprietary/product/app/Photos/Photos.apk
-rm -rf proprietary/product/app/PrebuiltGmail/PrebuiltGmail.apk
-rm -rf proprietary/product/priv-app/PrebuiltBugle/PrebuiltBugle.apk
-rm -rf proprietary/product/priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk
+# Clone local_manifests repository
+git clone https://github.com/shinichi-c/local_manifests_oplus --depth 1 .repo/local_manifests
 
-
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
-# Sync the repositories
+# Resync
 /opt/crave/resync.sh
 
-# Remove existing directories
-rm -rf device/oneplus/fajita
-rm -rf device/oneplus/sdm845-common
-rm -rf kernel/oneplus/sdm845
-rm -rf vendor/oneplus/fajita
-rm -rf vendor/oneplus/sdm845-common
-rm -rf hardware/oneplus
-
-# Clone repositories
-git clone https://github.com/uwu-gl/kernel_oneplus_sdm845 --depth=1 -b fifteen kernel/oneplus/sdm845
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 vendor/oneplus/fajita
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sdm845-common --depth=1 vendor/oneplus/sdm845-common
-git clone https://github.com/uwu-gl/device_oneplus_fajita --depth=1 -b fifteen device/oneplus/fajita
-git clone https://github.com/uwu-gl/device_oneplus_sdm845-common --depth=1 -b fifteen device/oneplus/sdm845-common
-git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 hardware/oneplus
-
+# Lunch
 source build/envsetup.sh
 lunch yaap_fajita-ap3a-userdebug
-m yaap
+m bacon
