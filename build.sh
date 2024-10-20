e9ba242f1b792054de817b54b7c514ccbf42b68f
kudo <Gokus12456@gmail.com>
2024-10-20 14:53:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ rm -rf .repo/local_manifests/
 
 # Repo Init
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs
 
 # Clone local_manifests repository
 git clone https://github.com/shinichi-c/local_manifests_oplus --depth 1 .repo/local_manifests
