6f8c23eee65a1d487fb3b53639c30220d106331f
kudo <Gokus12456@gmail.com>
2024-10-21 19:31:16 +0530

    Update pick.sh

--- pick.sh
+++ pick.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/shinichi-c/los-repo/refs/heads/Test/picks.txt
+wget https://raw.githubusercontent.com/shinichi-c/los-repo/refs/heads/Test/picks.sh
 chmod +x picks.txt
-. picks.txt
+. picks.sh
