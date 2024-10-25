ba4eeb1cc209123e42308152e773e7ff0fca5bf0
kudo <Gokus12456@gmail.com>
2024-10-25 14:16:50 +0530

    Update and rename pick.sh to gms.sh

---
+++ gms.sh
@@ -0,0 +1,3 @@
+rm -rf gms.sh
+wget https://raw.githubusercontent.com/shinichi-c/build_construction/refs/heads/gms.sh/gms.sh
+chmod +x gms.sh && . gms.sh
--- pick.sh
+++
@@ -1,4 +0,0 @@
-rm -rf picks.sh
-wget https://raw.githubusercontent.com/shinichi-c/los-repo/refs/heads/Test/picks.sh
-source build/envsetup.sh
-chmod +x picks.sh && . picks.sh
