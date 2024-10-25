feae797594f3668bc8ae55ac7e86b1aab28de013
kudo <Gokus12456@gmail.com>
2024-10-25 19:41:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Repo Init
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs
+repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs
 
-# Resync
+# Sync the repositories
 /opt/crave/resync.sh
 
 # Remove existing directories
@@ -18,21 +18,23 @@ rm -rf hardware/oneplus
 # Clone repositories #
 
 # Device
-git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b lineage-22.0 device/oneplus/fajita
-git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b lineage-22.0 device/oneplus/sdm845-common
+git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b pixelage device/oneplus/fajita
+git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b pixelage_d device/oneplus/sdm845-common
 
 # Vendor
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 -b lineage-22.0 vendor/oneplus/fajita
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sdm845-common --depth=1 -b lineage-22.0 vendor/oneplus/sdm845-common
 
 # Kernel
-git clone https://github.com/LineageOS/android_kernel_oneplus_sdm845 --depth=1 -b lineage-21 kernel/oneplus/sdm845
+git clone https://github.com/Terminator-J/crdroid_kernel_oneplus_sdm845 --depth=1 -b 15.0-test kernel/oneplus/sdm845
 
 # Hardware
-git clone https://github.com/LineageOS/android_hardware_oneplus --depth=1 -b lineage-22.0 hardware/oneplus
+git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 -b pixelage hardware/oneplus
+
+curl https://raw.githubusercontent.com/shinichi-c/build_construction/refs/heads/gms2.sh/gms.sh | bash
 
 # Lunch
+export PIXELAGE_BUILD="fajita"
 source build/envsetup.sh
-curl https://raw.githubusercontent.com/shinichi-c/build_construction/refs/heads/pick.sh/pick.sh | bash
-lunch lineage_fajita-ap3a-userdebug
-m bacon
+lunch pixelage_fajita-ap3a-userdebug
+mka bacon
