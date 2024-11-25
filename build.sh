af6672914a16181224e58c209c4176bd0e0357ed
kudo <Gokus12456@gmail.com>
2024-11-25 13:07:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,6 +19,9 @@ rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sdm845/display
 rm -rf device/lineage/sepolicy
 rm -rf device/qcom/sepolicy_vndr/legacy-um
+rm -rf external/libhybris
+rm -rf vendor/lindroid
+rm -rf external/lxc
 
 # Clone repositories #
 
@@ -44,6 +47,11 @@ git clone https://github.com/shinichi-c/display --depth=1 -b upstream hardware/q
 # Sepolicy
 git clone https://github.com/shinichi-c/android_device_pixelage_sepolicy --depth=1 device/lineage/sepolicy
 git clone https://github.com/shinichi-c/android_device_qcom_sepolicy_vndr --depth=1 -b lineage-22.0-legacy-um device/qcom/sepolicy_vndr/legacy-um
+
+# lindroid
+git clone https://github.com/Linux-on-droid/libhybris --depth=1 -b lindroid-21 external/libhybris
+git clone https://github.com/Linux-on-droid/vendor_lindroid --depth=1 -b lindroid-21 vendor/lindroid
+git clone https://github.com/Linux-on-droid/external_lxc --depth=1 -b lindroid-21 external/lxc
 
 # Lunch
 export PIXELAGE_BUILD="fajita"
