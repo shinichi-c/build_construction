1778c2c4d507f91ab20d40e36e6ce4686dadfece
kudo <Gokus12456@gmail.com>
2025-02-25 13:55:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ git clone https://github.com/Linux-on-droid/vendor_lindroid --depth=1 -b lindroi
 git clone https://github.com/Linux-on-droid/external_lxc --depth=1 -b lindroid-21 external/lxc
 
 # framework
-git clone https://github.com/shinichi-c/android_frameworks_native_axion --depth=1 -b lineage-22.1 frameworks/native
+git clone https://github.com/shinichi-c/frameworks_native_pixelos --depth=1 -b fifteen frameworks/native
 git clone https://github.com/shinichi-c/android_frameworks_base_axion --depth=1 -b lineage-22.1 frameworks/base
 
 # Lunch
