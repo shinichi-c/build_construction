bb9c4c9801a0ffcb5cf16bf66cd4bd3485b61afa
kudo <Gokus12456@gmail.com>
2025-02-25 21:08:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -61,6 +61,31 @@ git clone https://github.com/Linux-on-droid/external_lxc --depth=1 -b lindroid-2
 git clone https://github.com/shinichi-c/frameworks_native_pixelos --depth=1 -b fifteen frameworks/native
 git clone https://github.com/shinichi-c/frameworks_base --depth=1 -b fifteen frameworks/base
 
+# gms script
+
+# Configuration
+FILE_URL="https://raw.githubusercontent.com/shinichi-c/Gms/pixelage/common-vendor.mk"
+OUTPUT_DIR="vendor/gms/common"  # Target directory
+OUTPUT_FILE="common-vendor.mk"   # Filename
+OUTPUT_PATH="${OUTPUT_DIR}/${OUTPUT_FILE}"  # Full path
+
+# Create directory (if it doesn't exist)
+sudo mkdir -p "$OUTPUT_DIR"
+
+# Delete old file (if exists)
+sudo rm -f "$OUTPUT_PATH"
+
+# Download the new file to the target directory
+sudo wget "$FILE_URL" -O "$OUTPUT_PATH"
+
+# Check if download succeeded
+if [ $? -eq 0 ]; then
+  echo "Success: File saved to $OUTPUT_PATH"
+else
+  echo "Error: Failed to download. Check permissions, URL, or network."
+  exit 1
+fi
+
 # Lunch
 source build/envsetup.sh
 lunch aosp_fajita-ap4a-userdebug
