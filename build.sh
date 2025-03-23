0857c4cfcbe80512577eb59b5d60901403ddba9f
kudo <Gokus12456@gmail.com>
2025-03-23 13:46:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -53,6 +53,11 @@ git clone https://github.com/shinichi-c/android_device_qcom_sepolicy_vndr --dept
 git clone https://github.com/shinichi-c/android_frameworks_native_pixelage --depth=1 -b 15-Qpr2 frameworks/native
 git clone https://github.com/shinichi-c/android_frameworks_base_pixelage --depth=1 -b 15-Qpr2 frameworks/base
 
+# lindroid
+git clone https://github.com/Linux-on-droid/libhybris --depth=1 -b lindroid-21 external/libhybris
+git clone https://github.com/Linux-on-droid/vendor_lindroid --depth=1 -b lindroid-22.1 vendor/lindroid
+git clone https://github.com/Linux-on-droid/external_lxc --depth=1 -b lindroid-21 external/lxc
+
 # Lunch
 export PIXELAGE_BUILD="fajita"
 source build/envsetup.sh
