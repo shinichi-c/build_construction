0f04010bc9345acd78e7dcbc5a44014b3468039e
kudo <Gokus12456@gmail.com>
2025-03-24 22:10:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ rm -rf external/lxc
 git clone https://gitlab.com/kei-space/clang/r522817 prebuilts/clang/host/linux-x86/
 
 # Device
-git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b lineage-22.2 device/oneplus/fajita
+git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b lindroid device/oneplus/fajita
 git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b Rebase_Qpr2 device/oneplus/sdm845-common
 
 # Vendor
