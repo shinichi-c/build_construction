13b28571bb20718dce711bb809d0986976479f08
kudo <Gokus12456@gmail.com>
2025-06-16 09:35:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Repo Init
-repo init -u https://github.com/shinichi-c/android_manifest.git -b 15 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
 
 # Sync the repositories
 /opt/crave/resync.sh
@@ -19,26 +19,23 @@ rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sdm845/display
 rm -rf device/lineage/sepolicy
 rm -rf device/qcom/sepolicy_vndr/legacy-um
-rm -rf frameworks/native
-rm -rf frameworks/base
-rm -rf external/libhybris
-rm -rf vendor/lindroid
-rm -rf external/lxc
+rm -rf hardware/qcom-caf/bootctrl
+rm -rf vendor/qcom/opensource/dataservices
 
 # Clone repositories #
 # Clang
 git clone https://gitlab.com/kei-space/clang/r522817 prebuilts/clang/host/linux-x86/
 
 # Device
-git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b lindroid device/oneplus/fajita
-git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b Rebase_Qpr2 device/oneplus/sdm845-common
+git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b Evo_16 device/oneplus/fajita
+git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b Evo_16 device/oneplus/sdm845-common
 
 # Vendor
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 -b lineage-22.0 vendor/oneplus/fajita
 git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common --depth=1 -b elf_checks vendor/oneplus/sdm845-common
 
 # Kernel
-git clone https://github.com/Terminator-J/crdroid_kernel_oneplus_sdm845 --depth=1 -b 14.0-4.19-test2 kernel/oneplus/sdm845
+git clone https://github.com/shinichi-c/android_kernel_qcom_sdm845 --depth=1 -b LineageOS-22.1 kernel/oneplus/sdm845
 
 # Hardware
 git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 -b pixelage hardware/oneplus
@@ -50,20 +47,15 @@ git clone https://github.com/shinichi-c/display --depth=1 -b upstream hardware/q
 
 # Sepolicy
 git clone https://github.com/shinichi-c/android_device_pixelage_sepolicy --depth=1 device/lineage/sepolicy
-git clone https://github.com/shinichi-c/android_device_qcom_sepolicy_vndr --depth=1 -b lineage-22.0-legacy-um device/qcom/sepolicy_vndr/legacy-um
+git clone https://github.com/shinichi-c/android_device_qcom_sepolicy_vndr --depth=1 -b lineage-22.2-legacy-um device/qcom/sepolicy_vndr/legacy-um
 
-# framework
-git clone https://github.com/shinichi-c/android_frameworks_native_pixelage --depth=1 -b 15-Qpr2 frameworks/native
-git clone https://github.com/shinichi-c/android_frameworks_base_pixelage --depth=1 -b 15-Qpr2 frameworks/base
+# bootctrl
+git clone https://github.com/shinichi-c/android_hardware_qcom_bootctrl --depth=1 -b lineage-22.2-caf hardware/qcom-caf/bootctrl
 
-# lindroid
-git clone https://github.com/shinichi-c/libhybris --depth=1 -b lindroid-21 external/libhybris
-git clone https://github.com/shinichi-c/vendor_lindroid --depth=1 -b lindroid-22.1 vendor/lindroid
-git clone https://github.com/Linux-on-droid/external_lxc --depth=1 -b lindroid-21 external/lxc
+git clone https://github.com/shinichi-c/android_vendor_qcom_opensource_dataservices --depth=1 -b lineage-22.2 vendor/qcom/opensource/dataservices
 
-# Lunch
+#lunch
 export PIXELAGE_BUILD="fajita"
 source build/envsetup.sh
-lunch pixelage_fajita-bp1a-userdebug
-make installclean
+lunch lineage_fajita-bp2a-userdebug
 mka bacon
