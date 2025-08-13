e42aa231de00956ac3ff9c591e36a1ee86580d0d
kudo <Gokus12456@gmail.com>
2025-08-13 22:39:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ git clone https://github.com/shinichi-c/android_hardware_qcom_display --depth=1 
 
 # Sepolicy
 git clone https://github.com/shinichi-c/android_device_lineage_sepolicy --depth=1 -b evox-16 device/lineage/sepolicy
-git clone https://github.com/shinichi-c/android_device_qcom_sepolicy_vndr --depth=1 -b evox-16 device/qcom/sepolicy_vndr/legacy-um
+git clone https://github.com/shinichi-c/device_qcom_sepolicy_vndr --depth=1 -b bka-legacy-um device/qcom/sepolicy_vndr/legacy-um
 
 # bootctrl
 git clone https://github.com/shinichi-c/android_hardware_qcom_bootctrl --depth=1 -b lineage-22.2-caf hardware/qcom-caf/bootctrl
