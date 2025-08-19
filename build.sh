9a0dade41d43ceba15e54397c3f9b5ea01ba2eb5
kudo <Gokus12456@gmail.com>
2025-08-20 04:23:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_16 
 
 # Vendor
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 -b lineage-22.2 vendor/oneplus/fajita
-git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common --depth=1 -b c2-opcam vendor/oneplus/sdm845-common
+git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common --depth=1 -b c2-markv4 vendor/oneplus/sdm845-common
 git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_camera.git --depth=1 vendor/oneplus/camera
 
 # Kernel
