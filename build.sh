21418e1ca7e2e388e377d5c6e57d518b9cadc158
kudo <Gokus12456@gmail.com>
2025-08-23 00:04:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -55,8 +55,8 @@ git clone https://github.com/shinichi-c/device_qcom_sepolicy_vndr --depth=1 -b b
 git clone https://github.com/shinichi-c/android_hardware_qcom-caf_common --depth=1 -b markv1 hardware/qcom-caf/common
 
 # framework
-git clone https://github.com/shinichi-c/frameworks_native-L --depth=1 -b 16n frameworks/native
-git clone https://github.com/shinichi-c/frameworks_base-L --depth=1 -b 16 frameworks/base
+git clone https://github.com/shinichi-c/android_frameworks_native --depth=1 -b 16 frameworks/native
+git clone https://github.com/shinichi-c/android_frameworks_base_d --depth=1 -b 16 frameworks/base
 
 # lindroid
 git clone https://github.com/Linux-on-droid/libhybris --depth=1 -b tmp external/libhybris
