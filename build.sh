24f3589a56a8b3d38b35938cd6b12e3a04898ac9
kudo <Gokus12456@gmail.com>
2025-09-18 10:52:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ rm -rf frameworks/base
 # Clone repositories #
 
 # Device
-git clone https://github.com/shinichi-c/android_device_oneplus_fajita_16 --depth=1 -b c2-markv2-L device/oneplus/fajita
+git clone https://github.com/shinichi-c/android_device_oneplus_fajita_16 --depth=1 -b Evo_16 device/oneplus/fajita
 git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_16 --depth=1 -b c2-markv7_E device/oneplus/sdm845-common
 
 # Vendor
