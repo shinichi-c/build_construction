d7ffd84bf31a6094ab42a0c95e4b0b8b969a0560
kudo <Gokus12456@gmail.com>
2025-11-27 21:57:20 +0530

    Update kernel repository source in build script

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common
 git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_camera.git --depth=1 vendor/oneplus/camera
 
 # Kernel
-git clone https://github.com/shinichi-c/android_kernel_qcom_sdm845 --depth=1 -b markv5 kernel/oneplus/sdm845
+git clone https://github.com/EdwinMoq/android_kernel_oneplus_sdm845 --depth=1 -b lineage-23.1-4.19 kernel/oneplus/sdm845
 
 # Hardware
 git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 -b Qpr1-markv4_E hardware/oneplus
