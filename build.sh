37ecfd3692baa579865f0214fd1caa4829a38acc
kudo <Gokus12456@gmail.com>
2026-02-02 19:30:30 +0530

    Update vendor repository for oneplus_sdm845-common

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_16 
 
 # Vendor
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 -b lineage-22.2 vendor/oneplus/fajita
-git clone https://github.com/EdwinMoq/proprietary_vendor_oneplus_sdm845-common --depth=1 -b lineage-23.2-4.19 vendor/oneplus/sdm845-common
+git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common --depth=1 -b Qpr2_markv6 vendor/oneplus/sdm845-common
 
 # Kernel
 git clone https://github.com/shinichi-c/android_kernel_oneplus_sdm845 --depth=1 -b markv6 kernel/oneplus/sdm845
