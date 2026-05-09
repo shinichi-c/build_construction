c9ef5d07f4ffb1caacf58ba3b60c5b3861d4c7ec
kudo <Gokus12456@gmail.com>
2026-05-09 12:36:22 +0530

    Update branch name for cloning device repository

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ rm -rf frameworks/base
 # Clone repositories #
 
 # Device
-git clone https://github.com/shinichi-c/android_device_oneplus_fajita_16 --depth=1 -b Qpr2-mark device/oneplus/fajita
+git clone https://github.com/shinichi-c/android_device_oneplus_fajita_16 --depth=1 -b Qpr2-mark2 device/oneplus/fajita
 git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_16 --depth=1 -b Qpr2-markv20_E device/oneplus/sdm845-common
 
 # Vendor
