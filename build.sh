5395a2e4d366cc6ef30e813540289c1d5f134d50
kudo <Gokus12456@gmail.com>
2026-05-09 13:45:33 +0530

    Comment out sepolicy related commands in build.sh
    Comment out sepolicy removal and cloning commands.

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ rm -rf hardware/oneplus
 rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sm8250/audio
 rm -rf hardware/qcom-caf/sm8250/display
-rm -rf device/lineage/sepolicy
+# rm -rf device/lineage/sepolicy
 rm -rf device/qcom/sepolicy_vndr/legacy-um
 rm -rf hardware/qcom-caf/common
 rm -rf external/libhybris
@@ -49,7 +49,7 @@ git clone https://github.com/shinichi-c/android_hardware_qcom_audio --depth=1 -b
 git clone https://github.com/shinichi-c/android_hardware_qcom_display --depth=1 -b markE2 hardware/qcom-caf/sm8250/display
 
 # Sepolicy
-git clone https://github.com/shinichi-c/device_evolution_sepolicy.git --depth=1 -b bq2 device/lineage/sepolicy
+# git clone https://github.com/shinichi-c/device_evolution_sepolicy.git --depth=1 -b bq2 device/lineage/sepolicy
 git clone https://github.com/shinichi-c/android_device_qcom_sepolicy_vndr --depth=1 -b lineage-23.2-legacy-um device/qcom/sepolicy_vndr/legacy-um
 
 # qcom-caf_common
