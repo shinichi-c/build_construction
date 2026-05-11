00af31f5224e6ebd8d8fb4f67a6b64a22a37c938
kudo <Gokus12456@gmail.com>
2026-05-11 23:50:31 +0530

    Modify build script for device and hardware cloning
    Updated repository URLs and branches for device and hardware components.

--- build.sh
+++ build.sh
@@ -12,10 +12,8 @@ rm -rf device/oneplus/fajita
 rm -rf device/oneplus/sdm845-common
 rm -rf vendor/oneplus/fajita
 rm -rf vendor/oneplus/sdm845-common
-rm -rf vendor/oneplus/camera
 rm -rf kernel/oneplus/sdm845
 rm -rf hardware/oneplus
-rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sm8250/audio
 rm -rf hardware/qcom-caf/sm8250/display
 # rm -rf device/lineage/sepolicy
@@ -31,7 +29,7 @@ rm -rf frameworks/base
 
 # Device
 git clone https://github.com/shinichi-c/android_device_oneplus_fajita_16 --depth=1 -b Qpr2-mark2 device/oneplus/fajita
-git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_16 --depth=1 -b Qpr2-markv20_E device/oneplus/sdm845-common
+git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_16 --depth=1 -b markv21E device/oneplus/sdm845-common
 
 # Vendor
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 -b lineage-22.2 vendor/oneplus/fajita
@@ -44,7 +42,6 @@ git clone https://github.com/shinichi-c/android_kernel_oneplus_sdm845 --depth=1 
 git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 -b Qpr2-markv5_E hardware/oneplus
 
 # Hals
-git clone https://github.com/shinichi-c/android_hardware_qcom_audio --depth=1 -b markv1 hardware/qcom-caf/sdm845/audio
 git clone https://github.com/shinichi-c/android_hardware_qcom_audio --depth=1 -b markE2 hardware/qcom-caf/sm8250/audio
 git clone https://github.com/shinichi-c/android_hardware_qcom_display --depth=1 -b markE2 hardware/qcom-caf/sm8250/display
 
